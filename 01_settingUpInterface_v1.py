546e1176ef2decdb06b25dc03b92836aa46bede4
srslyy <kans@middleton.school.nz>
2023-05-12 11:01:09 +1200

    01_settingUpInterface_v1

---
+++ 01_settingUpInterface_v1.py
@@ -0,0 +1,17 @@
+"""Version 1 of Setting up interface for Maori Numbers and Colours Quiz
+Creating a GUI by importing and using tkinter"""
+
+from tkinter import *
+root = Tk()
+
+# Giving the window a title
+root.title("Maori Numbers and Colours Quiz")
+
+# Setting the window size
+root.geometry("1000x600")
+root.maxsize(1200, 650)
+
+# Changing the window's logo
+root.wm_iconbitmap('question_mark.png')
+
+root.mainloop()
---
+++ trial.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+
+def say_hi():
+    Label(root, bg="yellow", fg="red", text="Hi User!", font=("Times", "50", "bold")).pack()
+
+
+root = Tk()
+root.title("My first button")
+root.minsize(200, 100)
+
+# Creating a button object - with call to the function
+button1 = Button(root, text="Say hi!", command=say_hi)
+button1.pack()
+
+root.mainloop()
