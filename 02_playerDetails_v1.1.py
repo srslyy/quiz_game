a6c7f60aad20b483b0f74357e0c3b365cb8db3fc
srslyy <kans@middleton.school.nz>
2023-05-26 11:10:31 +1200

    Update 02_playerDetails_v1.1.py

--- 02_playerDetails_v1.1.py
+++ 02_playerDetails_v1.1.py
@@ -29,34 +29,34 @@ root = Tk()
 root.title("Maori Numbers and Colours Quiz")
 
 # Setting the window size
-root.geometry("1000x600")
+root.geometry("600x450")
 root.maxsize(1200, 650)
 
 # Creating the design for the welcome screen
 root.configure(bg="orange")
-heading = Label(root, bg="dark orange", fg="black", text="Maori Quiz", font=("Calibri", 50))
-heading.grid(row=1, column=1, columnspan=2, pady=30)
+heading = Label(root, bg="dark orange", fg="black", text="Maori Quiz", font=("Calibri", 40))
+heading.grid(row=1, column=1, columnspan=2, pady=10)
 
 welcome = Label(root, bg="orange", fg="black", text="WELCOME", font=("Calibri", 30, "bold"))
-welcome.grid(row=2, column=1, columnspan=2, pady=40)
+welcome.grid(row=2, column=1, columnspan=2, pady=20)
 
 # Start button - calls on function that closes window and opens the next
 startButton = Button(root, bg="dark orange", fg="black", text="START", width=10, height=1, command=comment,
                      font=("Calibri", 20))
-startButton.grid(row=5, column=1, columnspan=2, pady=90)
+startButton.grid(row=5, column=1, columnspan=2, pady=70)
 
 # Entering player details
 name_label = Label(root, bg="orange", text="Player Name:", font=("Calibri", 15))
-name_label.grid(row=3, sticky=E, column=1)
+name_label.grid(row=3, sticky=W, column=1, padx=10)
 
 age_label = Label(root, bg="orange", text="Player Age:", font=("Calibri", 15))
-age_label.grid(row=4, sticky=E, column=1)
+age_label.grid(row=4, sticky=W, column=1, padx=10)
 
-name = Entry(root)
-name.grid(row=3, column=2, pady=20)
+name = Entry(root, width=45)
+name.grid(row=3, column=2, pady=15)
 
-age = Entry(root)
-age.grid(row=4, column=2, pady=10)
+age = Entry(root, width=45)
+age.grid(row=4, column=2, pady=15)
 
 root.grid_columnconfigure(0, weight=1)
 root.grid_columnconfigure(6, weight=1)
